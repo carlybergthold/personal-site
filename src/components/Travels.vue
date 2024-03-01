ab07b17fc214b20eea4734e3b30ebe4e59706681
Carly Bergthold <carlybergthold@gmail.com>
2024-03-01 14:54:57 -0600

    New Orleans blog

--- src/components/Travels.vue
+++ src/components/Travels.vue
@@ -81,13 +81,79 @@
               <p>Immersed in simple delights like befriending the neighborhood cat, lunch break strolls on the beach, and movie nights, we often ignore our surroundings in favor of rest and relaxation. But, on occasion, we go out.</p>
               <p>For me, walking down unfamiliar roads is a holy experience (prepare thyself, for the ocean bringeth out my existentialism). Wordsworth took deep pleasure in the simple act of walking:</p>
               <p>To muse, to creep, to halt at will, to gaze.</p>
-              <p>This rings true in nature, as we pad down trails leading to pristine beaches, sand dunes, and coastal lakes, but it’s proved out perhaps even more on the sidewalks around our condo, ringed with a pedestrian milieu of surf shops, seafood restaurants, and kitschy attractions.</p>
+              <p>This rings true in nature, as we pad down trails leading to pristine beaches, sand dunes, and coastal lakes, but it's proved out perhaps even more on the sidewalks around our condo, ringed with a pedestrian milieu of surf shops, seafood restaurants, and kitschy attractions.</p>
               <p>Musings come easily in the off season.</p>
             </div>
           </div>
-          <div v-if="activeTab == 2">
-            <div class="has-text-weight-bold">New Orleans</div>
-            <div>February 1st - March 2nd</div>
+          <div v-if="activeTab == 2" class="has-text-left ml-2 mr-2">
+            <div class="location-container">
+              <div class="has-text-weight-bold has-text-centered">New Orleans</div>
+              <div class="has-text-centered">February 1st - March 2nd</div>
+              <img src="@/assets/images/nola/airbnb.jpeg" class="blog-image">
+              <div class="image-description">We stayed on the top floor of this townhouse</div>
+            </div>
+            <div class="dropdown strikethrough" @click="showNolaVignettes = !showNolaVignettes">
+              Highlights
+              <b-icon pack="fas" :key="showNolaVignettes" :icon="showNolaVignettes ? 'angle-up' : 'angle-down'" size="is-small"></b-icon>
+            </div>
+            <div v-if="showNolaVignettes">
+              <div><b>Mardi Gras</b></div>
+              <p>Mardi Gras is much more than just Fat Tuesday! This city partied for a solid two+ weeks, and we got in on as much as possible. I do not typically like parades. I could (and did!) watch Mardi Gras parades daily, for hours. Just grab a street beer, watch the festivities, and vibe. An unofficial slogan/song of Mardi Gras is “Do whatcha wanna,” and we complied. As if all this wasn't fun enough, our friends Brittany and Tyler came down for the weekend, which really put Mardi Gras over the top!</p>
+              <div class="image-container">
+                <div>
+                  <img src="@/assets/images/nola/mardi-gras-c-and-c.jpeg" class="blog-image">
+                  <div class="image-description">Getting into the Carnival spirit</div>
+                </div>
+                <div>
+                  <img src="@/assets/images/nola/mardi-gras-toilet.jpeg" class="blog-image">
+                  <div class="image-description">Hoping for toilet "drops" at the Krewe of Tucks parade</div>
+                </div>
+                <div>
+                  <img src="@/assets/images/nola/mardi-gras-mess.jpeg" class="blog-image">
+                  <div class="image-description">I don't envy the city cleaners</div>
+                </div>
+                <div>
+                  <img src="@/assets/images/nola/mardi-gras-us-four.jpeg" class="blog-image">
+                  <div class="image-description">Britt and Tyler</div>
+                </div>
+              </div>
+              <div><b>Swamp tour</b></div>
+              <p>Ever since watching the <a href="https://archer.fandom.com/wiki/Airboat" target="_blank">swamp episode</a> of Archer, I've wanted to ride in an airboat, and luckily, our experience didn't involve falling into the water or getting attacked by an alligator. Alligators are more active during warmer months, but we saw several (including a dead, bloated one - RIP Popeye). Even if we hadn't, the trip would have been worth it - the bayou is magical and full of plants, birds, and natural beauty. Our airboat captain also had some sick driving skills!</p>
+              <div class="image-container">
+                <div>
+                <img src="@/assets/images/nola/swamp-birds.jpeg" class="blog-image">
+                <div class="image-description">Birds of the bayou</div>
+                </div>
+                <div>
+                <img src="@/assets/images/nola/swamp-cam.jpeg" class="blog-image">
+                <div class="image-description">Cam and a lil' gator</div>
+                </div>
+              </div>
+              <div><b>Abita Brewery + Mystery House</b></div>
+              <p>Since we braved the Lake Pontchartrain Causeway, the longest continuous overwater bridge in the world, to drop by Abita Brewing company, we rounded out the trip by visiting the <a href="https://abitamysteryhouse.com/" target="_blank">Abita Mystery House</a>. You can enter through an old gas station and gaze upon hundreds of thousands of art pieces (trinkets? trash? UFOs???) made by artist John Preble. It truly must be seen to be believed.</p>
+              <div class="image-container">
+                <div>
+                <img src="@/assets/images/nola/abita-house.jpeg" class="blog-image">
+                <div class="image-description">Our new friend</div>
+                </div>
+                <div>
+
+                <img src="@/assets/images/nola/abita-house-2.jpeg" class="blog-image">
+                <div class="image-description">Should I decorate my office like this?</div>
+                </div>
+              </div>
+              <div><i>Honorable Mention: LA</i></div>
+              <p>Cameron had a work event in LA one weekend, so obviously I tagged along. Highlights include the view from Griffith Observatory before I was soaked by a rain storm (I thought those didn't exist in SoCal?), my first Korean spa experience, and my face hurting from laughing so much at a Tig Notaro comedy show where Ellen Degeneres was the surprise opener. I can't leave out my lunch from <a href="https://www.eatbrokenmouth.com/" target="_blank">Broken Mouth</a>, the #1 rated restaurant on Yelp - I could honestly eat this Korean Hawaiaan comfort food daily (the spam musubi is to die for!).</p>
+              <div class="image-container">
+                <div>
+                <img src="@/assets/images/nola/griffith-observatory.jpeg" class="blog-image">
+                <div class="image-description">Hollywood sign before the storm</div>
+                </div>
+              </div>
+              <div><i>Honorable Mention 2: Video games</i></div>
+              <p>A mystery illness knocked us both out for nearly two weeks, so I had to give an honorable mention to our newly acquired Steam Deck. As an absolute video game newbie, I used my down time to graduate from only being able to look at the ground/sky to being able to run around and shoot bad guys. Until they shoot me. Or I fall off a mountain.</p>
+              <p>Favorite games so far: Binding of Isaac: Rebirth, Borderlands 3, Unravel Two</p>
+            </div>
           </div>
           <div v-if="activeTab == 3">
             <div class="has-text-weight-bold">Santa Fe</div>
@@ -109,10 +175,10 @@ export default {
   title: 'Carly Bergthold - Travels',
   data() {
       return {
-          activeTab: 1,
+          activeTab: 2,
           showPcbThoughts: false,
           showPcbVignettes: false,
-          icon: 'angle-up'
+          showNolaVignettes: false
       };
   },
   methods: {
@@ -129,16 +195,6 @@ export default {
       } else {
         return 'orange-border'
       }
-    },
-    getIconWhenSomeRequirementIsMet() {
-      const properties = {
-        pack: 'fas',
-        icon: this.showPcbVignettes ? 'angle-up' : 'angle-down'
-      };
-
-      console.log(properties.icon)
-
-      return properties;
     }
   }
 }
@@ -187,6 +243,10 @@ p {
 }
 .pink-border {
   border: 1px solid var(--pink);
+  border-radius: 10px;
+}
+.yellow-border {
+  border: 1px solid var(--yellow);
   border-radius: 10px;
 }
 .dropdown {
