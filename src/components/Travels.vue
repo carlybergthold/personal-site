00cd3ce5f73d525bb88d31b3b5e9da5c31fa77f1
Carly Bergthold <carlybergthold@gmail.com>
2024-04-19 11:39:07 -0700

    Santa Fe

--- src/components/Travels.vue
+++ src/components/Travels.vue
@@ -6,19 +6,24 @@
         <div class="tabs-container has-text-weight-bold has-text-white"
           :class="$mq.above(640) ? 'tabs-container-desktop' : 'tabs-container-mobile'">
           <div @click='selectTab(1)' class="pink-background tab">{{ $mq.above(640) ? 'FLORIDA' : 'FL' }}
+            <b-icon v-if="activeTab == 1" pack="fas" icon="angle-double-left" size="is-small"></b-icon>
           </div>
           <div @click='selectTab(2)' class="yellow-background tab">{{ $mq.above(640) ? 'LOUISIANA' : 'LA' }}
-            <b-icon pack="fas" icon="angle-double-left" size="is-small"></b-icon>
+            <b-icon v-if="activeTab == 2" pack="fas" icon="angle-double-left" size="is-small"></b-icon>
           </div>
-          <div @click='selectTab(3)' class="teal-background tab">{{ $mq.above(640) ? 'NEW MEXICO' : 'NM' }}</div>
-          <div @click='selectTab(4)' class="orange-background tab">{{ $mq.above(640) ? 'ARIZONA' : 'AZ' }}</div>
+          <div @click='selectTab(3)' class="teal-background tab">{{ $mq.above(640) ? 'NEW MEXICO' : 'NM' }}
+            <b-icon v-if="activeTab == 3" pack="fas" icon="angle-double-left" size="is-small"></b-icon>
+          </div>
+          <div @click='selectTab(4)' class="orange-background tab">{{ $mq.above(640) ? 'ARIZONA' : 'AZ' }}
+            <b-icon v-if="activeTab == 4" pack="fas" icon="angle-double-left" size="is-small"></b-icon>
+          </div>
         </div>
         <div class="m-4" :class="getBorderStyle()">
           <div v-if="activeTab == 1" class="has-text-left ml-2 mr-2">
             <div class="location-container">
               <div class="has-text-weight-bold has-text-centered">Panama City Beach</div>
               <div class="has-text-centered">January 1st - February 1st</div>
-              <img src="@/assets/images/pcb/florida-condo.jpeg" class="blog-image">
+              <img src="@/assets/images/pcb/florida-condo.jpeg" class="vertical-blog-image">
               <div class="image-description">Our little condo a quarter mile from the beach</div>
             </div>
             <div class="dropdown strikethrough" @click="showPcbVignettes = !showPcbVignettes">
@@ -30,11 +35,11 @@
               <p>As long as it wasn't raining, freezing, or blustery (hello windburn!), we made the trek to the beach every work day after lunch. This early in the year, the water is too cold to get in, but you can join the retirees dotting the shore in shell hunting, staring into the roiling sea, and, of course, giving "The Nod and Wave" when you pass by.</p>
               <div class="image-container">
                 <div>
-                  <img src="@/assets/images/pcb/beach-pier.jpeg" class="blog-image">
+                  <img src="@/assets/images/pcb/beach-pier.jpeg" class="vertical-blog-image">
                   <div class="image-description">Public beach access gate 36. ifyky</div>
                 </div>
                 <div>
-                  <img src="@/assets/images/pcb/cam-beach.jpeg" class="blog-image">
+                  <img src="@/assets/images/pcb/cam-beach.jpeg" class="vertical-blog-image">
                   <div class="image-description">Cam protected against the winter weather</div>
                 </div>
               </div>
@@ -43,12 +48,12 @@
               <div class="image-container">
                 <div>
 
-                <img src="@/assets/images/pcb/skittles-royale.jpeg" class="blog-image">
+                <img src="@/assets/images/pcb/skittles-royale.jpeg" class="vertical-blog-image">
                 <div class="image-description">The man himself</div>
                 </div>
                 <div>
 
-                <img src="@/assets/images/pcb/skittles-royale-house.jpeg" class="blog-image">
+                <img src="@/assets/images/pcb/skittles-royale-house.jpeg" class="vertical-blog-image">
                 <div class="image-description">Senior's house</div>
                 </div>
               </div>
@@ -57,12 +62,12 @@
               <div class="image-container">
                 <div>
 
-                <img src="@/assets/images/pcb/grayton-dune.jpeg" class="blog-image">
+                <img src="@/assets/images/pcb/grayton-dune.jpeg" class="vertical-blog-image">
                 <div class="image-description">Cam by the sand dunes</div>
                 </div>
                 <div>
 
-                <img src="@/assets/images/pcb/grayton-shells.jpeg" class="blog-image">
+                <img src="@/assets/images/pcb/grayton-shells.jpeg" class="vertical-blog-image">
                 <div class="image-description">Part of the path was made of shells!</div>
                 </div>
               </div>
@@ -88,7 +93,7 @@
             <div class="location-container">
               <div class="has-text-weight-bold has-text-centered">New Orleans</div>
               <div class="has-text-centered">February 1st - March 2nd</div>
-              <img src="@/assets/images/nola/airbnb.jpeg" class="blog-image">
+              <img src="@/assets/images/nola/airbnb.jpeg" class="vertical-blog-image">
               <div class="image-description">We stayed on the top floor of this townhouse</div>
             </div>
             <div class="dropdown strikethrough" @click="showNolaVignettes = !showNolaVignettes">
@@ -100,19 +105,19 @@
               <p>Mardi Gras is much more than just Fat Tuesday! This city partied for a solid two+ weeks, and we got in on as much as possible. I do not typically like parades. I could (and did!) watch Mardi Gras parades daily, for hours. Just grab a street beer, watch the festivities, and vibe. An unofficial slogan/song of Mardi Gras is “Do whatcha wanna,” and we complied. As if all this wasn't fun enough, our friends Brittany and Tyler came down for the weekend, which really put Mardi Gras over the top!</p>
               <div class="image-container">
                 <div>
-                  <img src="@/assets/images/nola/mardi-gras-c-and-c.jpeg" class="blog-image">
+                  <img src="@/assets/images/nola/mardi-gras-c-and-c.jpeg" class="vertical-blog-image">
                   <div class="image-description">Getting into the Carnival spirit</div>
                 </div>
                 <div>
-                  <img src="@/assets/images/nola/mardi-gras-toilet.jpeg" class="blog-image">
+                  <img src="@/assets/images/nola/mardi-gras-toilet.jpeg" class="vertical-blog-image">
                   <div class="image-description">Hoping for toilet "drops" at the Krewe of Tucks parade</div>
                 </div>
                 <div>
-                  <img src="@/assets/images/nola/mardi-gras-mess.jpeg" class="blog-image">
+                  <img src="@/assets/images/nola/mardi-gras-mess.jpeg" class="vertical-blog-image">
                   <div class="image-description">I don't envy the city cleaners</div>
                 </div>
                 <div>
-                  <img src="@/assets/images/nola/mardi-gras-us-four.jpeg" class="blog-image">
+                  <img src="@/assets/images/nola/mardi-gras-us-four.jpeg" class="vertical-blog-image">
                   <div class="image-description">Britt and Tyler</div>
                 </div>
               </div>
@@ -120,11 +125,11 @@
               <p>Ever since watching the <a href="https://archer.fandom.com/wiki/Airboat" target="_blank">swamp episode</a> of Archer, I've wanted to ride in an airboat, and luckily, our experience didn't involve falling into the water or getting attacked by an alligator. Alligators are more active during warmer months, but we saw several (including a dead, bloated one - RIP Popeye). Even if we hadn't, the trip would have been worth it - the bayou is magical and full of plants, birds, and natural beauty. Our airboat captain also had some sick driving skills!</p>
               <div class="image-container">
                 <div>
-                <img src="@/assets/images/nola/swamp-birds.jpeg" class="blog-image">
+                <img src="@/assets/images/nola/swamp-birds.jpeg" class="vertical-blog-image">
                 <div class="image-description">Birds of the bayou</div>
                 </div>
                 <div>
-                <img src="@/assets/images/nola/swamp-cam.jpeg" class="blog-image">
+                <img src="@/assets/images/nola/swamp-cam.jpeg" class="vertical-blog-image">
                 <div class="image-description">Cam and a lil' gator</div>
                 </div>
               </div>
@@ -132,12 +137,12 @@
               <p>Since we braved the Lake Pontchartrain Causeway, the longest continuous overwater bridge in the world, to drop by Abita Brewing company, we rounded out the trip by visiting the <a href="https://abitamysteryhouse.com/" target="_blank">Abita Mystery House</a>. You can enter through an old gas station and gaze upon hundreds of thousands of art pieces (trinkets? trash? UFOs???) made by artist John Preble. It truly must be seen to be believed.</p>
               <div class="image-container">
                 <div>
-                <img src="@/assets/images/nola/abita-house.jpeg" class="blog-image">
+                <img src="@/assets/images/nola/abita-house.jpeg" class="vertical-blog-image">
                 <div class="image-description">Our new friend</div>
                 </div>
                 <div>
 
-                <img src="@/assets/images/nola/abita-house-2.jpeg" class="blog-image">
+                <img src="@/assets/images/nola/abita-house-2.jpeg" class="vertical-blog-image">
                 <div class="image-description">Should I decorate my office like this?</div>
                 </div>
               </div>
@@ -145,7 +150,7 @@
               <p>Cameron had a work event in LA one weekend, so obviously I tagged along. Highlights include the view from Griffith Observatory before I was soaked by a rain storm (I thought those didn't exist in SoCal?), my first Korean spa experience, and my face hurting from laughing so much at a Tig Notaro comedy show where Ellen Degeneres was the surprise opener. I can't leave out my lunch from <a href="https://www.eatbrokenmouth.com/" target="_blank">Broken Mouth</a>, the #1 rated restaurant on Yelp - I could honestly eat this Korean Hawaiaan comfort food daily (the spam musubi is to die for!).</p>
               <div class="image-container">
                 <div>
-                <img src="@/assets/images/nola/griffith-observatory.jpeg" class="blog-image">
+                <img src="@/assets/images/nola/griffith-observatory.jpeg" class="vertical-blog-image">
                 <div class="image-description">Hollywood sign before the storm</div>
                 </div>
               </div>
@@ -154,13 +159,76 @@
               <p>Favorite games so far: Binding of Isaac: Rebirth, Borderlands 3, Unravel Two</p>
             </div>
           </div>
-          <div v-if="activeTab == 3">
-            <div class="has-text-weight-bold">Santa Fe</div>
-            <div>March 10th - April 13th</div>
+          <div v-if="activeTab == 3" class="has-text-left ml-2 mr-2">
+              <div class="location-container">
+                <div class="has-text-weight-bold has-text-centered">Santa Fe</div>
+                <div class="has-text-centered">March 10th - April 13th</div>
+                <img src="@/assets/images/santafe/airbnb.jpeg" class="horizontal-blog-image">
+                <div class="image-description">Our little adobe house near the Railyard</div>
+              </div>
+            <div class="dropdown strikethrough" @click="showSFVignettes = !showSFVignettes">
+              Highlights
+              <b-icon pack="fas" :key="showSFVignettes" :icon="showSFVignettes ? 'angle-up' : 'angle-down'" size="is-small"></b-icon>
+            </div>
+            <div v-if="showSFVignettes">
+              <div><b>Hiking</b></div>
+              <p>My number one goal in Santa Fe was to be on a mountain as much as possible, and we more than accomplished that! There are excellent trails right in Santa Fe as well as easy day trips.</p>
+              <p>At Bandelier National Monument, you can climb ladders to visit the ancient homes of Ancestral Puebloans, which are carved into the rockface. Some of the taller ladders are honestly terrifying, but I had to climb them after seeing some 7-year-olds coming down.</p>
+              <div class="image-container">
+                <div>
+                  <img src="@/assets/images/santafe/bandelier-ladder.jpeg" class="vertical-blog-image">
+                  <div class="image-description">A cave at Bandelier National Monument</div>
+                </div>
+                <div>
+                  <img src="@/assets/images/santafe/dale-ball-hike.jpeg" class="vertical-blog-image">
+                  <div class="image-description">Dale Ball trails, Santa Fe</div>
+                </div>
+              </div>
+              <p>Another fun one for lovers of heights is the Rio Grande Gorge near Taos, where I was convinced the wind would push me into the river. The gorge is just a few minutes away from the <a href="https://earthship.com/visit/" target="_blank">Earthship community</a>, too, which are very interesting to visit. Earthships are built out of recycled materials like tires, cans, and bottles. Some grow their own food, treat their own sewage, and collect power from the sun and wind. Taos is an adorable little town, and we had a great time walking the main square, visiting Taos Mesa Brewery, and getting a short hike in at Devisadero Peak.</p>
+              <div class="image-container">
+                <div>
+                  <img src="@/assets/images/santafe/gorge.jpeg" class="horizontal-blog-image">
+                  <div class="image-description">Rio Grande Gorge, Taos</div>
+                </div>
+                <div>
+                  <img src="@/assets/images/santafe/upper-bandelier-me.jpeg" class="horizontal-blog-image">
+                  <div class="image-description">Frijoles Canyon Rim Trail, Los Alamos</div>
+                </div>
+              </div>
+              <p>My favorite hike in Santa Fe proper was Mount Atalaya. While the trek is only 7 miles, it's <i>steep</i> (my Apple watch clocked an elevation gain of over 2000 feet). We took a wrong turn at some point and ended up scaling the snowy side of the mountain before realizing the actual path was just up the hill. We made it to the summit unscathed and grateful for amazing views (I recently got Lasik and discovered mountain views are even better when you can see more than blobs).</p>
+              <div><b>Food</b></div>
+              <p>I have to say, I was not expecting the food in Santa Fe to be so insanely good. The star of the show is <s>the chili pepper</s> New Mexican food, which is full of spice and flavor and cheese and corn (we really enjoyed Tune Up Cafe and El Comal). We also had amazing meals at Jambo Cafe (African), Paper Dosa (Indian), and Izanami (Japanese).</p>
+              <div class="image-container">
+                <div>
+                <img src="@/assets/images/santafe/izanami.jpeg" class="horizontal-blog-image">
+                <div class="image-description">Izanami's "tatami" room</div>
+                </div>
+                <div>
+                <img src="@/assets/images/santafe/enchiladas.jpeg" class="vertical-blog-image">
+                <div class="image-description">A few of the 1 million enchiladas I consumed in NM</div>
+                </div>
+              </div>
+              <div><b>Ojo Santa Fe</b></div>
+              <p>Northern New Mexico is known for its hot springs, so obviously we had to partake. The grounds at Ojo were gorgeous! We went on a cold, windy day, so we were scuttling from pool to pool as fast as our flip flops could carry us, but it was amazing being nice and toasty in the water as snow flurries fell in the afternoon.</p>
+              <div><b>Datil</b></div>
+              <p>One weekend we went to see some of my relatives in Datil, NM. They have a huge plot of land in the middle of nowhere, complete with herds of elk, antelope, and the occasional coyote. Us city slickers got a crash course on identifying scat, which I have used on every hike since.</p>
+              <div class="image-container">
+                <div>
+                <img src="@/assets/images/santafe/hike-thru-history.jpeg" class="vertical-blog-image">
+                <div class="image-description">Aunt Nancy and I by petroglyphs in Reserve, NM</div>
+                </div>
+              </div>
+              <div><b>Meow Wolf</b></div>
+              <p>An enigma. All I can say is, if you ever find yourself in Santa Fe, this is a must see!</p>
+            </div>
           </div>
-          <div v-if="activeTab == 4">
-            <div class="has-text-weight-bold">Sedona</div>
-            <div>April 16th - May 18th</div>
+          <div v-if="activeTab == 4" class="has-text-left ml-2 mr-2">
+              <div class="location-container">
+                <div class="has-text-weight-bold has-text-centered">Sedona</div>
+                <div>April 16th - May 18th</div>
+                <!-- <img src="@/assets/images/nola/airbnb.jpeg" class="vertical-blog-image">
+                <div class="image-description">Our little adobe house near the Railyard</div> -->
+              </div>
           </div>
         </div>
       </div>
@@ -174,10 +242,11 @@ export default {
   title: 'Carly Bergthold - Travels',
   data() {
       return {
-          activeTab: 2,
+          activeTab: 3,
           showPcbThoughts: false,
           showPcbVignettes: false,
-          showNolaVignettes: false
+          showNolaVignettes: false,
+          showSFVignettes: true
       };
   },
   methods: {
@@ -248,6 +317,14 @@ p {
   border: 1px solid var(--yellow);
   border-radius: 10px;
 }
+.teal-border {
+  border: 1px solid var(--teal);
+  border-radius: 10px;
+}
+.orange-border {
+  border: 1px solid var(--orange);
+  border-radius: 10px;
+}
 .dropdown {
   display: flex;
   align-items: center;
@@ -266,9 +343,14 @@ p {
   gap: 1rem;
   justify-content: center;
   text-align: center;
+  align-items: center;
 }
-.blog-image {
+.vertical-blog-image {
   width: 20rem;
+  border-radius: 10px;
+}
+.horizontal-blog-image {
+  width: 25rem !important;
   border-radius: 10px;
 }
 .image-description {
